60be3444bcedf008f6a4674ea2ea0a06d576640c
AlexKogachevsky <alex.kogachevsky@gmail.com>
2022-08-10 20:24:08 +0300

    feat: added html and js

---
+++ js/script.js
@@ -0,0 +1,24 @@
+'use strict'
+//number 1,2, +-Infinity, 1.2, NaN
+//string 'dog', "cat", `mouse`
+//boolean true, false
+//object {},[], null
+//undefined undefined
+//null ...0...
+//bigint 1n
+//symbol {}
+// console.log(typeof); //определение типа данных
+// ** возведение в степень, % остаток от деления, =,==,===
+// var (работает, даёт undefined),let (консоль работает только после объявления),const способ объявления переменных
+
+// let num = 1;
+// num = "string";
+// console.log(num);
+// // var a = 1
+// var a = 2
+
+// console.log(a);
+// var dog = "Bim";
+// console.log(dog);
+
+alert( 'Привет, мир!' );
